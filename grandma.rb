136c6b919b235e5afadf274b428cf873b3c96a2c
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-03 07:31:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(pharse)
      pharse ="I LOVE YOU TOO PUMPKIN!"
   elsif pharse =="Hi Nana, how are you?"
     pharse =="HUH?! SPEAK UP, SONNY!"
-  elsif pharse ="Hi!"
+  elsif pharse == "Hi!"
     pharse ="HUH?! SPEAK UP, SONNY!"
   elsif pharse == "WHAT DID YOU EAT TODAY?"
     pharse ="NO, NOT SINCE 1938!"

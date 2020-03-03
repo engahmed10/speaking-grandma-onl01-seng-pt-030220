14141c7986359a0f47a3f356b435fc88861df7f3
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-03 08:21:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,9 @@ def speak_to_grandma(pharse)
      pharse ="I LOVE YOU TOO PUMPKIN!"
   elsif pharse == pharse.capitalize
     pharse = "HUH?! SPEAK UP, SONNY!"
-  elsif pharse == 'WHAT DID YOU EAT TODAY?'
+  elsif pharse == pharse.upcase
     pharse = "NO, NOT SINCE 1938!"
-  elsif pharse == "WHAT?"
+  elsif pharse == pharse.upcase
     pharse = "NO, NOT SINCE 1938!"
   else
     pharse ="HUH?! SPEAK UP, SONNY!"
